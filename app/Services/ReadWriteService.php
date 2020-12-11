fb1a3a7edb2a8b4e530307ffcab4687785c93841
sm.steve.moretz@gmail.com <sm.steve.moretz@gmail.com>
2020-12-11 11:55:36 +0330

    Add Service Structure

---
+++ app/Services/ReadWriteService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+class ReadWriteService{
+
+    /**
+     * @return string
+     */
+    public function read(){
+        return '';
+    }
+
+    /**
+     * @param string $text
+     */
+    public function write($text){
+
+    }
+}
