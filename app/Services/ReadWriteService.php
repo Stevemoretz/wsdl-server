864cf8260b74a84ef08f214f4ea25bd95835711a
sm.steve.moretz@gmail.com <sm.steve.moretz@gmail.com>
2020-12-11 12:15:55 +0330

    Connect Service to Model to interact with DB

--- app/Services/ReadWriteService.php
+++ app/Services/ReadWriteService.php
@@ -2,19 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\ClientData;
+
 class ReadWriteService{
 
     /**
      * @return string
      */
     public function read(){
-        return '';
+        return ClientData::first()['data'];
     }
 
     /**
      * @param string $text
      */
     public function write($text){
-
+        ClientData::updateOrCreate(['id'=>1],['id'=>1,'data'=>$text]);
     }
 }
